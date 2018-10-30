0c4c41beeeb933dcb1a8c682d2af3f492a8977a4
lz <luiszumarraga95@gmail.com>
2018-10-30 14:22:59 +0100

    Departanamento

---
+++ 03-MVC-Act1/03-MVC-Act1/Models/clsDepartamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _03_MVC_Act1.Models {
+    public class clsDepartamento {
+
+        public String nombreDepartamento { get; set; }
+        public int idDepartamento { get; set; }
+
+        public clsDepartamento() {
+            this.nombreDepartamento = "";
+            this.idDepartamento = 0;
+        }
+
+        public clsDepartamento(String nombre, int idDepartamento) {
+            this.nombreDepartamento = nombre;
+            this.idDepartamento = idDepartamento;
+        }
+
+    }
+}
