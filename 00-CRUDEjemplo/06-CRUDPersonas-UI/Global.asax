178cffae6b9cc15b859a19817cf297860e130faa
lz <luiszumarraga95@gmail.com>
2018-11-06 14:23:34 +0100

    PAM

---
+++ 00-CRUDEjemplo/06-CRUDPersonas-UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_06_CRUDPersonas_UI.MvcApplication" Language="C#" %>
---
+++ 06-CRUDPersonas/06-CRUDPersonas-Entidades/clsPersona.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_CRUDPersonas_Entidades {
+    public class clsPersona
+    {
+        #region atributosYPropiedades
+
+        public int idPersona { get; set; }
+
+        public String nombre { get; set; }
+
+        public String apellidos { get; set; }
+
+        public DateTime fechaNacimiento { get; set; }
+
+        public String direccion { get; set; }
+
+        public String telefono { get; set; }
+
+
+        #endregion
+
+
+        #region constructores
+
+        public clsPersona(int idPersona, String nombre, String apellidos, DateTime fechaNacimiento, String direccion, String telefono) {
+
+            this.idPersona = idPersona;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.fechaNacimiento = fechaNacimiento;
+            this.direccion = direccion;
+            this.telefono = telefono;
+
+        }
+
+        public clsPersona() {
+
+            this.idPersona = 0;
+            this.nombre = "";
+            this.apellidos = "";
+            this.fechaNacimiento = new DateTime();
+            this.direccion = "";
+            this.telefono = "";
+
+        }
+
+        #endregion
+
+
+    }
+}
