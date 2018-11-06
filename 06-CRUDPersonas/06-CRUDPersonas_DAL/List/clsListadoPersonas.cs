b15d01a0e10c8aa77e2fd363e8c8e37f116f07c3
lz <luiszumarraga95@gmail.com>
2018-11-06 14:45:20 +0100

    commit

---
+++ 06-CRUDPersonas/06-CRUDPersonas_DAL/List/clsListadoPersonas.cs
@@ -0,0 +1,20 @@
+﻿using _06_CRUDPersonas_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_CRUDPersonas_DAL.List {
+    public class clsListadoPersonas {
+
+        public List<clsPersona> listadoCompletoPersonas() {
+            List<clsPersona> listaPersonas = new List<clsPersona>();
+
+            //TODO
+
+            return listaPersonas;
+        }
+
+    }
+}
