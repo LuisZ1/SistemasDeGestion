08714b0779805395bf9fefb306bb45570fc1ee40
lz <luiszumarraga95@gmail.com>
2018-11-29 14:17:16 +0100

    commit

--- 06-CRUDPersonas/06-CRUDPersonas_Entities/clsPersona.cs
+++ 06-CRUDPersonas/06-CRUDPersonas_Entities/clsPersona.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace _06_CRUDPersonas_Entities {
     public class clsPersona
     {
         #region atributosYPropiedades
 
+        [HiddenInput(DisplayValue = false)]
         public int idPersona { get; set; }
 
+        [Display(Name="Nombre")]
+        [Required(ErrorMessage = "Campo obligatorio")]
         public String nombre { get; set; }
 
+        [Display(Name = "Apellidos")]
+        [Required(ErrorMessage = "Campo obligatorio")]
         public String apellidos { get; set; }
 
+        [Display(Name = "Fecha de Nacimiento")]
+        [Required(ErrorMessage = "Campo obligatorio")]
         public DateTime fechaNacimiento { get; set; }
 
         public String direccion { get; set; }
 
+        //[DisplayFormat(DataFormatString = "0;(+34|0034|34)?[6|7|9] [0-9]{8}")]
+        [RegularExpression(@"(\+34|0034|34)?[6|7|9][0-9]{8}", ErrorMessage = "No eres tú, soy yo")]
         public String telefono { get; set; }
 
         public int idDepartamento { get; set; }
