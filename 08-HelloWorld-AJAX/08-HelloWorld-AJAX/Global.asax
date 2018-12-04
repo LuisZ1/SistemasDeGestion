3b0a676a213b907e97a487768c44be032fff1406
lz <luiszumarraga95@gmail.com>
2018-12-04 14:42:21 +0100

    AJAX

---
+++ 08-HelloWorld-AJAX/08-HelloWorld-AJAX/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_08_HelloWorld_AJAX.MvcApplication" Language="C#" %>
---
+++ 08-HelloWorld-AJAX/08-HelloWorld-AJAX/Scripts/metodosAJAX.js
@@ -0,0 +1,26 @@
+﻿window.onload = initEvents;
+
+function initEvents() {
+    document.getElementById("btnMostrarLista").addEventListener("click", llamadaAJAX, false);
+}
+
+
+function llamadaAJAX() {
+    var miLlamada = new XMLHttpRequest();
+    //miLlamada.open('GET', '/Home/Index');
+    miLlamada.open('GET', 'https://apirestpersonaslz.azurewebsites.net/api/personas');
+    
+    miLlamada.onreadystatechange = function () {
+        alert(miLlamada.readyState);
+        if (miLlamada.readyState < 4) {
+            //alert('He entrado');
+            document.getElementById('textoObjetivo').innerHTML = "cargando...";
+        } else {
+            if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+                document.getElementById('textoObjetivo').innerHTML = miLlamada.responseText;
+            }
+        }
+    };
+    
+    miLlamada.send();
+}
