377d5cf61f105aba282587d955373b30b93faf1e
lz <luiszumarraga95@gmail.com>
2018-12-11 14:28:35 +0100

    Preparando CRUD Personas

--- 08-HelloWorld-AJAX/08-HelloWorld-AJAX/Scripts/metodosAJAX.js
+++ 08-HelloWorld-AJAX/08-HelloWorld-AJAX/Scripts/metodosAJAX.js
@@ -1,10 +1,10 @@
 ﻿window.onload = initEvents;
 
 function initEvents() {
-    document.getElementById("btnMostrarLista").addEventListener("click", llamadaAJAX, false);
+    document.getElementById("btnMostrarLista").addEventListener("click", llamadaAJAX_v2, false);
 }
 
-
+//------------------------ LISTADO COMPLETO --------------------------------------------------------
 function llamadaAJAX() {
     var miLlamada = new XMLHttpRequest();
     //miLlamada.open('GET', '/Home/Index');
@@ -13,7 +13,7 @@ function llamadaAJAX() {
     miLlamada.onreadystatechange = function () {
         alert(miLlamada.readyState);
         if (miLlamada.readyState < 4) {
-            //alert('He entrado');
+            
             document.getElementById('textoObjetivo').innerHTML = "cargando...";
         } else {
             if (miLlamada.readyState == 4 && miLlamada.status == 200) {
@@ -23,4 +23,40 @@ function llamadaAJAX() {
     };
     
     miLlamada.send();
+}
+
+//------------------------CON UN OBJETO PERSONA----------------------------------------------
+
+function llamadaAJAX_v2() {
+    var miLlamada = new XMLHttpRequest();
+    //miLlamada.open('GET', '/Home/Index');
+    miLlamada.open('GET', 'https://apirestpersonaslz.azurewebsites.net/api/personas');
+    
+    miLlamada.onreadystatechange = function () {
+        if (miLlamada.readyState < 4) {
+            document.getElementById('textoObjetivo').innerHTML = "cargando...";
+        } else {
+            if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+
+                var miPersona = new Persona();
+                var arrayPersonas = JSON.parse(miLlamada.responseText);
+                document.getElementById('textoObjetivo').innerHTML = arrayPersonas[0].nombre + " " + arrayPersonas[0].apellidos;
+
+            }
+        }
+    };
+    
+    miLlamada.send();
+}
+
+class Persona {
+    constructor(idPersona, nombre, apellidos, fechaNacimiento, direccion, telefono, idDepartamento) {
+        this.idPersona = idPersona;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.idDepartamento = idDepartamento;
+    }
 }
