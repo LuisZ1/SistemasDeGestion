a6a400d529645c861e16b4e6d1b522aa6d17dd5d
lz <luiszumarraga95@gmail.com>
2019-01-22 14:35:11 +0100

    empezando order-details

--- Workspace Angular/hamburguer-menu-v2/src/app/app.module.ts
+++ Workspace Angular/hamburguer-menu-v2/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { OrderDetailsComponent } from './order-details/order-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    OrderDetailsComponent
   ],
   imports: [
     BrowserModule,
